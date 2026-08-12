2f0f4eebd2ba24bc1fb9e968c5d099dc7861b14d
Inessa <iapugina@mail.ru>
2026-08-12 10:40:42 +0300

    12-1.2

--- js/utils.js
+++ js/utils.js
@@ -25,11 +25,11 @@ export const showDataError = ()=> {
 
 };
 
-export debounce = (callback, timeoutDelay = DELAY) => {
-  let timeoutId;
-  return (...rest) => {
-    clearTimeout(timeoutId);
-    timeoutId = setTimeout(() => callback.apply(this, rest), timeoutDelay);
-  };
+// export debounce = (callback, timeoutDelay = DELAY) => {
+//   let timeoutId;
+//   return (...rest) => {
+//     clearTimeout(timeoutId);
+//     timeoutId = setTimeout(() => callback.apply(this, rest), timeoutDelay);
+//   };
 
-};
+// };
