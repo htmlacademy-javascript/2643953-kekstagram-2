9c337a8b873889f811d83b87553046a80d6463cd
Inessa <iapugina@mail.ru>
2026-08-15 10:30:12 +0300

    12-2.2

--- js/utils.js
+++ js/utils.js
@@ -3,17 +3,6 @@ const dataErrorTemplate = document
   .querySelector("#data-error")
   .content.querySelector(".data-error");
 
-// const getRandomPositiveInteger = (a, b) => {
-//   const lower = Math.ceil(Math.min(Math.abs(a), Math.abs(b)));
-//   const upper = Math.floor(Math.max(Math.abs(a), Math.abs(b)));
-//   const result = Math.random() * (upper - lower + 1) + lower;
-//   return Math.floor(result);
-// };
-
-// const getRandomArrayElement = (elements) => elements[getRandomPositiveInteger(0, elements.length - 1)];
-
-// export {getRandomPositiveInteger, getRandomArrayElement};
-
 export const showDataError = () => {
   const dataErrorNode = dataErrorTemplate.cloneNode(true);
   document.body.append(dataErrorNode);
